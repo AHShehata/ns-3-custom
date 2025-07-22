1e74ac349c2aae6ead67667fc01a10d3e93759f4
AHShehata <ayahanyshehata@gmail.com>
2025-07-22 15:05:57 +0200

    Update wifi7_DL_legacy_3OBSS.cc

--- scratch/wifi7_DL_legacy_3OBSS.cc
+++ scratch/wifi7_DL_legacy_3OBSS.cc
@@ -71,19 +71,7 @@
  #include "ns3/rng-seed-manager.h"
  #include <chrono>
  
- 
- 
- // This is an example of  how to configure an IEEE 802.11be Wi-Fi network.
- // The simulation assumes a configurable number of stations in an infrastructure network:
- //  STA     AP
- //    *     *
- //    |     |
- //   n1     n2
- //
- // Packets in this simulation belong to BestEffort Access Class (AC_BE).
- // By selecting an acknowledgment sequence for DL MU PPDUs, it is possible to aggregate a
- // Round Robin scheduler to the AP, so that DL MU PPDUs are sent by the AP via DL OFDMA.
- 
+
  
  using namespace ns3;
  NS_LOG_COMPONENT_DEFINE("eht-wifi-networktrial");
